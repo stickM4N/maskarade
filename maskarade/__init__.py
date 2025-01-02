68110e7a41ff382cb4c14b6d1bbee0eb4d0ff308
stickm4n <stickm4n.mail@gmail.com>
2025-01-02 22:46:53 +0100

    feat: add model utils

--- maskarade/__init__.py
+++ maskarade/__init__.py
@@ -2,6 +2,7 @@ from .connector import Event, EventChannel, ModelConnector
 from .log import ModelLogger
 from .model import ModelBase, model_ref_associations
 from .reference import ModelRef
+from .utils import final_models, make_model_class, model_from_ref, model_ref_from_ref
 
 
 __version__ = '0.0.0'
